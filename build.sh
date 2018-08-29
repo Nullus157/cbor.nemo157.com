45b9653362944bc54fa9686475cabf4cad96e9ac
Wim Looman <wim@nemo157.com>
2018-08-29 21:22:44 +0200

    Remove old build during build

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 set -e -o pipefail
 cargo +nightly build --target wasm32-unknown-unknown
+rm -rf build/*
 wasm-bindgen target/wasm32-unknown-unknown/debug/cbor_nemo157_com.wasm --out-dir build
 rm -rf dist/*
 npm run build
