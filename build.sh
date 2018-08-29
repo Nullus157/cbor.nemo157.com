20f37e5be32f5613bb768f747b0ad6b1e218628a
Wim Looman <wim@nemo157.com>
2018-08-29 21:29:26 +0200

    Use release build for smaller wasm size

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e -o pipefail
-cargo +nightly build --target wasm32-unknown-unknown
+cargo +nightly build --target wasm32-unknown-unknown --release
 rm -rf build/*
-wasm-bindgen target/wasm32-unknown-unknown/debug/cbor_nemo157_com.wasm --out-dir build
+wasm-bindgen target/wasm32-unknown-unknown/release/cbor_nemo157_com.wasm --out-dir build
 rm -rf dist/*
 npm run build
